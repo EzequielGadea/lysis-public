a65efa39c03c4932073aa6a11a7840a71c271990
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-18 06:18:55 -0300

    Cambios.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Http;
+
+class UserController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $http = Http::post('http://localhost:8001/api/auth', [
+            'email' => $request->post('email'),
+            'password' => $request->post('password'),
+            'remember' => false,
+        ]);
+        
+        $apiResponse = json_decode($http, true);
+        if ($apiResponse['result'] !== 'Ok') {
+            return back()->with('statusLogin', $apiResponse['result']);
+        }
+
+        return view('index');
+    }
+
+    public function getRegister()
+    {
+        $http = Http::get('http://localhost:8001/getSubscriptions');
+        $apiResponse = json_decode($http, true);
+
+        return view('registerModal', [
+            'subscriptions' => $apiResponse,
+        ]);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $http = Http::post('http://localhost:8001/api/register', $request->validated());
+        $apiResponse = json_decode($http, true);
+        if ($apiResponse['result'] !== 'ok') {
+            return back()->with('statusRegister', $apiResponse);
+        }
+        return route('home');
+    }
+
+    public function showLogin()
+    {
+        return view('login');
+    }
+}
